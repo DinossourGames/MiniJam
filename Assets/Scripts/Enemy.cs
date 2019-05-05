dcf2880aa7e916829ca1c6cbbebfd2fd41e7edfe
Alef <alef@dinossour.com.br>
2019-05-05 02:03:16 -0300

    Jammy Animations by mika added to project player core movementa and anims done player health damage added

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Transform player;
+    public float stopDistance = 1f;
+    public int speed;
+    private Rigidbody2D rb;
+
+    public float MaxKnockDistance = 10;
+    private float atualKnock;
+    private Vector2 vetor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+        atualKnock = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KnockCheck();
+
+        if (GameObject.FindGameObjectsWithTag("Player").Length > 0)
+        {
+
+            if (Vector2.Distance(transform.position, player.position) > stopDistance && player)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            collision.collider.GetComponent<Player>().TakeDamage(1);
+        }
+    }
+
+
+    private void KnockCheck()
+    {
+        if (rb.velocity != Vector2.zero)
+        {
+            if (atualKnock >= MaxKnockDistance)
+            {
+                atualKnock = 0;
+                rb.velocity = Vector2.zero;
+
+            }
+            else
+            {
+                atualKnock = Vector2.Distance(transform.position, player.position);
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+    public int maxHp = 5;
+    private int currentHp;
+
+    private Animator animator;
+    private Rigidbody2D rb;
+    private Vector2 moveAmmount;
+
+    private bool isFacingRight;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        currentHp = maxHp;
+        isFacingRight = true;
+    }
+
+    void Update()
+    {
+        CheckLife();
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Z))
+            animator.SetTrigger("Provoque");
+
+        if (movement != Vector2.zero)
+        {
+            if (movement.x < 0)
+            {
+                isFacingRight = false;
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+            else
+            {
+                isFacingRight = true;
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+            moveAmmount = movement.normalized * speed;
+            animator.SetBool("Running", true);
+
+        }
+        else
+            animator.SetBool("Running", false);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Ass"))
+            rb.MovePosition(rb.position + moveAmmount * Time.deltaTime);
+
+    }
+
+    private void CheckLife()
+    {
+        if (currentHp <= 0)
+            Die();
+    }
+
+    internal void TakeDamage(object damage)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+        Debug.Log("Morreu");
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (maxHp > 0)
+        {
+            currentHp -= damage;
+            Debug.Log(currentHp.ToString());
+        }
+
+    }
+}
