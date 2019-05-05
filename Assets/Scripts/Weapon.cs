cfcd3a12c6c98b6a093e7062ed62596152f3195b
Alef <alef@dinossour.com.br>
2019-05-05 04:22:28 -0300

    Simple projectile script projectile prefab shooting is working camera position fixed

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public GameObject projectile;
+    public Transform shotPoint;
+    public float timeBetweenShots;
+
+    private float shotTime;
+
+
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            if (Time.time >= shotTime)
+            {
+                Instantiate(projectile, shotPoint.position, transform.rotation);
+                //cameraAnim.SetTrigger("shake");
+                shotTime = Time.time + timeBetweenShots;
+            }
+        }
+
+
+    }
+}
